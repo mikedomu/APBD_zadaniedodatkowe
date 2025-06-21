eddb5812012f2b5b534d5987ecfd240859e2672a
Michal Domurat <michal.domurat03@gmail.com>
2025-06-21 23:41:33 +0200

    updated desription

--- AdditionalProject/AdditionalProject/Models/DTOs/EventEverythingDto.cs
+++ AdditionalProject/AdditionalProject/Models/DTOs/EventEverythingDto.cs
@@ -4,7 +4,7 @@ public class EventEverythingDto
 {
     public int IdEvent { get; set; }
     public string Name { get; set; } = null!;
-    public string? Description { get; set; } = null!;
+    public string? Description { get; set; }
     public DateTime Date { get; set; }
     public List<string> Speakers { get; set; } = new();
     public int RegisteredParticipants { get; set; }
